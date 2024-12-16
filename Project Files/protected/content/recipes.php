c8c82bf980c475410a194eb17b812633d73c0267
Pipa Csaba <csabapipa@gmail.com>
2024-12-16 16:13:51 +0100

    Commit: Testing and fixing the update operation Fixed the arising issues with updating recipes from the database Tested out the new function, which is working fine Made sure the sql command doesn't run before getting a submit value from the submit page

--- Project Files/protected/content/recipes.php
+++ Project Files/protected/content/recipes.php
@@ -48,13 +48,16 @@ function modify(){
          require VIEWS_DIR.'recipes/modify.php';
     }
     else{
+        $id = get_p();
         $recipenameM = filter_input(INPUT_POST, 'recipename', FILTER_SANITIZE_STRING);
         $ingredientsM = filter_input(INPUT_POST, 'ingredients', FILTER_SANITIZE_STRING);
         $servingsM = filter_input(INPUT_POST, 'servings', FILTER_VALIDATE_INT);
         $instructionsM = filter_input(INPUT_POST, 'instructions', FILTER_SANITIZE_STRING);
+
+        $result = executeStatement('UPDATE recipes SET recipename = :recipenameM, ingredients = :ingredientsM, servings = :servingsM, instructions = :instructionsM WHERE recipe_id = :recipe_id', 
+        [':recipenameM' => $recipenameM, ':ingredientsM' => $ingredientsM, ':servingsM' => $servingsM, ':instructionsM' => $instructionsM, ':recipe_id' => $id]);
     }
-    $result = executeStatement('UPDATE recipes SET recipename = :recipenameM, ingredients = :ingredientsM, servings = :servingsM, instructions = :instructionsM WHERE recipe_id = :recipe_id', 
-            ['recipenameM' => $recipenameM, 'ingredientsM' => $ingredientsM, 'servingsM' => $servingsM, 'instructionsM' => $instructionsM, 'recipe_id' => $recipe_id]);
+   
 }
 function add(){
       if(!filter_has_var(INPUT_POST, 'submit') || 
@@ -66,7 +69,9 @@ function add(){
         $ingredientsF = filter_input(INPUT_POST, 'ingredients', FILTER_SANITIZE_STRING);
         $servingsF = filter_input(INPUT_POST, 'servings', FILTER_VALIDATE_INT);
         $instructionsF = filter_input(INPUT_POST, 'instructions', FILTER_SANITIZE_STRING);
+
+        $result = executeStatement('INSERT INTO recipes (recipename, ingredients, servings, instructions) values (:recipenameF, :ingredientsF, :servingsF, :instructionsF)', 
+        [':recipenameF' => $recipenameF, ':ingredientsF' => $ingredientsF, ':servingsF' => $servingsF, ':instructionsF' => $instructionsF,]); 
     }
-    $result = executeStatement("INSERT INTO recipes ('recipename', 'ingredients', 'servings', 'instructions') values ('$recipenameF', '$ingredientsF', '$servingsF', '$instructionsF')"); 
 }
 ?>
