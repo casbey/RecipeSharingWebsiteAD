b0e0e93cccedb5d151e98bea187903ea35850900
Pipa Csaba <csabapipa@gmail.com>
2024-12-17 23:03:30 +0100

    Commit: Search view (#43) Created a better search box with bootstrap

--- Project Files/protected/views/recipes/search.php
+++ Project Files/protected/views/recipes/search.php
@@ -1,6 +1,22 @@
 <br/>
-<h2>Search for a recipe!</h2>
-<form action="" method="POST" accept-charset="UTF-8">
-    <input type="text" name="recipesearch" id="recipesearch_id" placeholder="Recipe name or an ingredient" required>
-    <button type="submit" name="submit" value="1">Search</button>
-</form>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-6">
+            <!-- Search Card -->
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <h2 class="text-center mb-4">Search for a recipe!</h2>
+                    <form action="" method="POST" accept-charset="UTF-8">
+                        <div class="input-group mb-3">   
+                            <input type="text" name="recipesearch" id="recipesearch_id" class="form-control form-control-lg" placeholder="Recipe name or an ingredient" required>
+                            <button type="submit" class="btn btn-primary btn-lg" name="submit" value="1">
+                                <i class="bi bi-search"></i>Search
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- End Search Card -->
+        </div>
+    </div>
+</div>
