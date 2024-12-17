e553ded953e341f3e3c0b67708a60ef4e4d0a6c2
Pipa Csaba <csabapipa@gmail.com>
2024-12-18 00:52:23 +0100

    Commit: Background colour fix (#49) Added a style tag in the index.php so that the background colour wouldn't change from brown to white

--- Project Files/index.php
+++ Project Files/index.php
@@ -13,7 +13,7 @@
     }?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color:rgb(216, 186, 151);">
     <?php include VIEWS_DIR.'header.php';?>
     <?php require PROTECTED_DIR.'menu.php';?>
     <?php require PROTECTED_DIR.'content.php';?>
