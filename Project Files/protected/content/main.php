a794942e3d660c0c8c4454cebd6f8476198baa24
Fodor Tamas <fodor.tamas2005@gmail.com>
2024-12-18 00:40:32 +0100

    Commit #8: main_style

--- Project Files/protected/content/main.php
+++ Project Files/protected/content/main.php
@@ -1,2 +1,15 @@
-<br/>
-<h2>Start page</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/main_style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body id="mainbody">
+    <br>
+    <h1 id="greet">The best place for the hungry!</h1>
+    <h1 id="sz1">Share your own recipes!</h1>
+    <h1 id="sz2">Cook up something delicious from other recipes that are from all around the world!</h1>
+</body>
+</html>
