0a417db924eae15c7b6aa8340b62d332260f27ac
Fodor Tamas <fodor.tamas2005@gmail.com>
2024-12-18 00:36:55 +0100

    Commit #7: header

--- Project Files/protected/views/header.php
+++ Project Files/protected/views/header.php
@@ -1,2 +1,14 @@
-<h1>Recipe Sharing Website</h1>
-<h2>Actions</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/header_style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body id="headerbody">
+<h1 id="headerleft">Recipe Sharing Website</h1>
+<h2 id="headerright"></h2>
+<img src="public/img/food.png">
+</body>
+</html>
